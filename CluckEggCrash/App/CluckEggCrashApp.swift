7b26adf1bebfa64f874eec313e471108387956f0
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-10-28 16:23:40 +0200

    init

---
+++ CluckEggCrash/App/CluckEggCrashApp.swift
@@ -0,0 +1,17 @@
+//
+//  CluckEggCrashApp.swift
+//  CluckEggCrash
+//
+//  Created by Serhii Babchuk on 28.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct CluckEggCrashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CELaunchView()
+        }
+    }
+}
---
+++ Pods/Target Support Files/Pods-CluckEggCrash/Pods-CluckEggCrash-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CluckEggCrash : NSObject
+@end
+@implementation PodsDummy_Pods_CluckEggCrash
+@end
